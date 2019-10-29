9147e42cbb59fbbd3e68a5faa75aa879ae89c5ab
Unknown <webbj19@ferris.edu>
2019-10-28 23:36:00 -0400

    added assets from outside project

---
+++ Assets/DroneControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneControl : MonoBehaviour
+{
+    public float speed = 3;
+    Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+
+        //transform.position += movement * speed;
+        body.AddForce(movement * speed);
+
+    }
+}
