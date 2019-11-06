ada63b3dcbea471eb4dd5227aee0a6ea4a7e26df
Unknown <webbj19@ferris.edu>
2019-11-06 18:49:58 -0500

    made scaler, changed map and made objects destroyer
    Added sclaler to cookie behavior
    Added a collides to destroy objects

--- Assets/CookieBehavior.cs
+++ Assets/CookieBehavior.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CookieBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
+    int scaler;
+    bool destroy = false;
+    public int rangeMin = 1;
+    public int rangeMax = 6;
+
     void Start()
     {
-        
+        scaler = Random.Range(rangeMin, rangeMax);
+        transform.localScale = new Vector3(scaler, scaler, scaler);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right  * Time.deltaTime * 20);
+        /**if (turn == false)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * 20);
+        }**/
+       // if (turn == true)
+       // {
+            transform.Translate(Vector3.right * Time.deltaTime * 20);
+
+       // }
+       if (destroy == true)
+        {
+Destroy(this.gameObject);
+            print("help");
+        }
+
+    }
+  void onTriggerEneter(Collider collider)
+    {
+        if (collider.transform.tag == "Destroy")
+        {
+            
+            print("hit");
+            destroy = true;
+
+        }
     }
 }
+
